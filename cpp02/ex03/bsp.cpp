d60eea9a5bfa345cae7625cc92c5688a8de83205
damachad <damachad@student.42porto.com>
2024-06-20 13:06:39 +0100

    Update bsp with cross product

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,62 +6,49 @@
 /*   By: damachad <damachad@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:05:12 by damachad          #+#    #+#             */
-/*   Updated: 2024/04/02 16:29:02 by damachad         ###   ########.fr       */
+/*   Updated: 2024/06/20 13:05:53 by damachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-bool is_on_edge(const Point a, const Point b, const Point c, const Point p)
-{
-	bool    abp_same_x = a.getX() == b.getX() && p.getX() == a.getX();
-    bool    acp_same_x = a.getX() == c.getX() && p.getX() == a.getX();
-    bool    cbp_same_x = c.getX() == b.getX() && p.getX() == c.getX();
-    bool    abp_same_y = a.getY() == b.getY() && p.getY() == a.getY();
-    bool    acp_same_y = a.getY() == c.getY() && p.getY() == a.getY();
-    bool    cbp_same_y = c.getY() == b.getY() && p.getY() == c.getY();
-    
-	if(abp_same_x || abp_same_y || acp_same_x || acp_same_y \
-    || cbp_same_x || cbp_same_y)
-		return (true);
-	return(false);
-}
-
-bool    is_vertex(Point const a, Point const b, Point const c, Point const p)
-{
-    bool    ap = (a.getX() == p.getX() && a.getY() == p.getY());
-    bool    bp = (b.getX() == p.getX() && b.getY() == p.getY());
-    bool    cp = (c.getX() == p.getX() && c.getY() == p.getY());
-    
-    if (ap || bp || cp)
-        return true;
-    return false;
-}
-
 bool    repeated(Point const a, Point const b, Point const c)
 {
-    bool    ab = (a.getX() == b.getX() && a.getY() == b.getY());
-    bool    ac = (a.getX() == c.getX() && a.getY() == c.getY());
-    bool    bc = (b.getX() == c.getX() && b.getY() == c.getY());
-    
-    if (ab || ac || bc)
-        return true;
-    return false;
+	bool    ab = (a.getX() == b.getX() && a.getY() == b.getY());
+	bool    ac = (a.getX() == c.getX() && a.getY() == c.getY());
+	bool    bc = (b.getX() == c.getX() && b.getY() == c.getY());
+	
+	if (ab || ac || bc)
+		return true;
+	return false;
+}
+
+Fixed crossProduct(const Point &A, const Point &B, const Point &P)
+{
+	Fixed ABx = B.getX() - A.getX();
+	Fixed ABy = B.getY() - A.getY();
+	Fixed APx = P.getX() - A.getX();
+	Fixed APy = P.getY() - A.getY();
+	
+	return (ABx * APy - ABy * APx);
 }
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-	Fixed ap_x = point.getX() - a.getX();
-    Fixed ap_y = point.getY() - a.getY();
+	// valid triangle check
+	if (repeated(a, b, c))
+		return false;
 
-    if (repeated(a, b, c) || is_vertex(a, b, c, point) || is_on_edge(a, b, c, point))
-        return false;
-    bool p_ab = ((b.getX() - a.getX()) * ap_y - (b.getY() - a.getY()) * ap_x) > 0;
+	Fixed cpABP = crossProduct(a, b, point);
+	Fixed cpBCP = crossProduct(b, c, point);
+	Fixed cpCAP = crossProduct(c, a, point);
 
-    if (((c.getX() - a.getX()) * ap_y - (c.getY() - a.getY()) * ap_x > 0) == p_ab) 
-        return false;
-    if (((c.getX() - b.getX()) * (point.getY() - b.getY()) - (c.getY() - b.getY())\
-	* (point.getX() - b.getX()) > 0) != p_ab) 
-        return false;
-    return true;
+	// point on edge or vertex
+	if (cpABP == 0 || cpBCP == 0 || cpCAP == 0)
+		return false;
+	// point on same side of line
+	if ((cpABP > 0 && cpBCP > 0 && cpCAP > 0) || \
+	(cpABP < 0 && cpBCP < 0 && cpCAP < 0))
+		return true;
+	return false;
 }
